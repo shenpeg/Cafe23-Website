5d4706583855ae1eb7a80af65d3d24f0668244c4
peggy <98036446+shenpeg@users.noreply.github.com>
2023-04-07 18:26:56 -0400

    employee view roles dropdown typo and starting about view

--- app/models/employee_authentication.rb
+++ app/models/employee_authentication.rb
@@ -1,7 +1,7 @@
 module EmployeeAuthentication 
 
   # For view dropdowns
-  ROLES_LIST = [['Employee', 0],['Manager', 1],['Administrator', 2]].freeze
+  ROLES_LIST = [['Employee', 1],['Manager', 2],['Administrator', 3]].freeze
   
   # login by username
   def Employee.authenticate(username, password)
