cfcfdc8a7b41962d83deec9bcbd70ce5fdf66e87
peggy <98036446+shenpeg@users.noreply.github.com>
2023-04-18 16:59:19 -0400

    bunch of small view changes

--- config/routes.rb
+++ config/routes.rb
@@ -22,8 +22,8 @@ Rails.application.routes.draw do
   # Payroll routes
   get 'payrolls/store_form', to: 'payrolls#store_form', as: :store_form
   get 'payrolls/employee_form', to: 'payrolls#employee_form', as: :employee_form
-  get 'payrolls/employee_payroll', to: 'payrolls#employee_payroll', as: :employee_payroll
-  get 'payrolls/store_payroll', to: 'payrolls#store_payroll', as: :store_payroll
+  post 'payrolls/employee_payroll', to: 'payrolls#employee_payroll', as: :employee_payroll
+  post 'payrolls/store_payroll', to: 'payrolls#store_payroll', as: :store_payroll
 
   # Resource routes (maps HTTP verbs to controller actions automatically):
   resources :stores, except: [:destroy]
