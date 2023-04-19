336ac2b8135d999d543e896739de0b1681ba25c2
peggy <98036446+shenpeg@users.noreply.github.com>
2023-04-19 01:37:07 -0400

    store payroll scenario for admins

--- app/controllers/payrolls_controller.rb
+++ app/controllers/payrolls_controller.rb
@@ -36,17 +36,17 @@ class PayrollsController <  ApplicationController
     authorize! :store_payroll, :payrolls_controller
 
     if current_user.manager_role?
-      store = current_user.current_assignment.store
+      @store = current_user.current_assignment.store
     end
     if current_user.admin_role?
-      store = Store.find(params[:store_id])
+      @store = Store.find(params[:store_id])
     end
 
     @start_date = Date.parse(params[:start_date])
     @end_date = Date.parse(params[:end_date])
     @calculator = PayrollCalculator.new(DateRange.new(@start_date, @end_date))
 
-    @store_payroll = @calculator.create_payrolls_for(store)
+    @store_payroll = @calculator.create_payrolls_for(@store)
   end
 
 end
